1e807f810fe52316abf679b7638c988ff27e699d
Ajay Raju <ajayrajuadapa@gmail.com>
2025-03-10 21:24:10 +0530

    Fixed errors and fixed icons

---
+++ src/fontawesome.js
@@ -0,0 +1,37 @@
+// Import the Font Awesome core
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+// Import the specific icons you're using
+import {
+  faCheckCircle,
+  faExclamationCircle,
+  faMapMarkerAlt,
+  faPhone,
+  faPhoneAlt,
+  faEnvelope,
+  faClock,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
+
+import {
+  faFacebookF,
+  faTwitter,
+  faLinkedinIn,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+
+// Add all icons to the library
+library.add(
+  faCheckCircle,
+  faExclamationCircle,
+  faMapMarkerAlt,
+  faPhone,
+  faPhoneAlt,
+  faEnvelope,
+  faClock,
+  faTimes,
+  faFacebookF,
+  faTwitter,
+  faLinkedinIn,
+  faInstagram
+);
